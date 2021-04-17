bcbcb8f979aa32de9c07af6396f1c0c6ff18f994
elton de campos <eltoncampos36@gmail.com>
2021-04-16 23:35:37 -0300

    chore: create auth route to check users

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,9 +1,74 @@
 const { Router } = require("express");
+const auth = require("../../middleare/auth");
+const bcrypt = require("bcryptjs");
+
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../../models/User");
 
 const authRouter = Router();
 
-authRouter.get("/", (req, res) => {
-  return res.send("auth route");
+authRouter.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    return res.json(user);
+  } catch (err) {
+    return res.status(500).send("Serve error");
+  }
 });
+
+authRouter.post(
+  "/",
+  [
+    check("email", "Insert a valid emal").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: "Invalid credentials" }] });
+      }
+
+      const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordMatched) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: "Invalid credentials" }] });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = authRouter;
