fdce851ee105f17873167f0fbbdcfb384b19d8f4
elton de campos <eltoncampos36@gmail.com>
2021-04-19 01:05:21 -0300

    chore: get github user route

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 const auth = require("../../middleare/auth");
 
@@ -283,4 +285,32 @@ profileRouter.delete("/education/:edu_id", auth, async (req, res) => {
     return res.status(500).send("Server error");
   }
 });
+
+profileRouter.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientID"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ message: "No Github profile found" });
+      }
+
+      return res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server error");
+  }
+});
+
 module.exports = profileRouter;
