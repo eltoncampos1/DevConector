e40421d3a62af9553ece1f6f00729a42b09a899c
elton de campos <eltoncampos36@gmail.com>
2021-04-18 21:31:55 -0300

    chore: get current user profile

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -1,9 +1,30 @@
 const { Router } = require("express");
+const auth = require("../../middleare/auth");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 const profileRouter = Router();
 
 profileRouter.get("/", (req, res) => {
   return res.send("profile route");
+});
+
+profileRouter.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avata"]);
+
+    if (!profile) {
+      return res.status(400).json({ message: "Profile does not exists." });
+    }
+
+    return res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server error");
+  }
 });
 
 module.exports = profileRouter;
